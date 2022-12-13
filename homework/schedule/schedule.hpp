d3bb45a465ee78accf91ee00deb072b7958a8ff2
Maciej Białczak <m.bialczak@protonmail.com>
2022-12-13 23:42:39 +0100

    Only template solution left

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -6,40 +6,6 @@
 #include <thread>
 #include <utility>
 
-// void schedule(std::function<void()> func, std::chrono::seconds time) {
-//     std::this_thread::sleep_for(time);
-//     func();
-// }
-
-// void schedule(std::function<void(int)> func,
-//               std::chrono::seconds time,
-//               int value) {
-//     std::this_thread::sleep_for(time);
-//     func(value);
-// }
-// void schedule(std::function<void(std::string, double)> func,
-//               std::chrono::seconds time,
-//               std::string text,
-//               double value) {
-//     std::this_thread::sleep_for(time);
-//     func(text, value);
-// }
-
-// template <typename... Args>
-// void schedule(std::function<void(Args...)>,
-//               std::chrono::seconds time,
-//               Args... args) {
-//     std::this_thread::sleep_for(time);
-//     func(std::forward<Args>(args)...);
-// }
-
-// template <typename... Args, typename Action>
-// void schedule(Action func,
-//               std::chrono::seconds time,
-//               Args... args) {
-//     std::this_thread::sleep_for(time);
-//     func(std::forward<Args>(args)...);
-// }
 template <typename... Args,
           typename Action = std::function<void(Args...)>>
 void schedule(Action func,
@@ -48,11 +14,3 @@ void schedule(Action func,
     std::this_thread::sleep_for(time);
     func(std::forward<Args>(args)...);
 }
-
-// template <typename... Args, typename std::function<void(Args...)> Action>
-// void schedule(Action func,
-//               std::chrono::seconds time,
-//               Args... args) {
-//     std::this_thread::sleep_for(time);
-//     func(std::forward<Args>(args)...);
-// }
