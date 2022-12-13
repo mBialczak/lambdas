d297e1b882178ad8c5c71d09616f3df68bbfc52d
Maciej Białczak <m.bialczak@protonmail.com>
2022-12-14 00:27:52 +0100

    swithced order of template params + invoke used

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -6,11 +6,10 @@
 #include <thread>
 #include <utility>
 
-template <typename... Args,
-          typename Action = std::function<void(Args...)>>
+template <typename Action, typename... Args>
 void schedule(Action func,
               std::chrono::seconds time,
               Args... args) {
     std::this_thread::sleep_for(time);
-    func(std::forward<Args>(args)...);
+    std::invoke(func, std::forward<Args>(args)...);
 }
